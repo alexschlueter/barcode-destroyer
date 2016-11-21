49f598b0dcef8c66c2769839e839ab3bafc14c3c
Aleksej Matis <a_mati01@uni-muenster.de>
2016-11-21 20:26:37 +0100

    GUI: first version of the GUI

--- CVPBarcode/mainwindow.cpp
+++ CVPBarcode/mainwindow.cpp
@@ -3,9 +3,69 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    setupUI();
+    this->showMaximized();
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void MainWindow::setupUI(){
+    mainWidget = new QWidget(this);
+    layout = new QGridLayout(mainWidget);
+    this->setCentralWidget(mainWidget);
+
+    //menubar
+    menubar = new QWidget(mainWidget);
+    layout->addWidget(menubar,0,0,1,2,Qt::AlignTop);
+    QHBoxLayout * menulayout = new QHBoxLayout(menubar);
+    menulayout->setMargin(0);
+    menulayout->setAlignment(Qt::AlignLeft);
+
+    pb_clear = new QPushButton("Clear");
+    menulayout->addWidget(pb_clear);
+
+    pb_import = new QPushButton("Import");
+    menulayout->addWidget(pb_import);
+
+    pb_solve = new QPushButton("Solve");
+    menulayout->addWidget(pb_solve);
+
+    //mainTable
+    mainTable = new QTableWidget();
+    mainTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    layout->addWidget(mainTable,1,0,1,2);
+
+
+    //stausbar
+    stausbar = new QWidget(mainWidget);
+    layout->addWidget(stausbar,2,0,1,2, Qt::AlignBottom);
+    QHBoxLayout * statuslayout = new QHBoxLayout(stausbar);
+    //statuslayout->setAlignment(Qt::AlignLeft);
+    lbl_status = new QLabel("Test");
+    statuslayout->addWidget(lbl_status,5,Qt::AlignLeft);
+    pb_status = new QProgressBar();
+    pb_status->setFixedWidth(150);
+    pb_status->setTextVisible(false);
+    statuslayout->addWidget(pb_status,1,Qt::AlignRight);
+    statuslayout->setMargin(0);
+
+    connect(pb_clear,SIGNAL(clicked(bool)),this,SLOT(setUp()));
+    connect(pb_import,SIGNAL(clicked(bool)),this,SLOT(addRow()));
+}
+
+void MainWindow::setUp(){
+    QStringList header;
+    header.append("Path");
+    header.append("Label");
+    mainTable->clear();
+    mainTable->setRowCount(0);
+    mainTable->setColumnCount(2);
+    mainTable->setHorizontalHeaderLabels(header);
+}
+
+void MainWindow::addRow(){
+    mainTable->setRowCount(mainTable->rowCount()+1);
 }
--- CVPBarcode/mainwindow.h
+++ CVPBarcode/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLayout>
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QProgressBar>
+#include <QTableWidget>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +16,26 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    QWidget * mainWidget;
+    QGridLayout * layout;
+    QWidget * menubar;
+    QWidget * stausbar;
+
+    QPushButton * pb_clear;
+    QPushButton * pb_import;
+    QPushButton * pb_solve;
+
+    QTableWidget * mainTable;
+
+    QLabel * lbl_status;
+    QProgressBar * pb_status;
+    void setupUI();
+
+private slots:
+    void addRow();
+    void setUp();
 };
 
 #endif // MAINWINDOW_H
