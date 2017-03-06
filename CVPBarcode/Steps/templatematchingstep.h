fe8aa975884bdbd47431472e052514ec045ae25b
Aleksej Matis <a_mati01@uni-muenster.de>
2017-03-06 22:36:46 +0100

    fix compile bug on linux

--- CVPBarcode/Steps/templatematchingstep.h
+++ CVPBarcode/Steps/templatematchingstep.h
@@ -190,7 +190,7 @@ struct ReadingResult {
     double cost;
 
     ReadingResult(cv::Point2f _leftBnd, cv::Point2f _rightBnd, std::vector<uchar> _scanLine)
-        : leftBnd(std::move(_leftBnd)), rightBnd(std::move(_rightBnd)), scanLine(std::move(_scanLine)), levels({}), cost(0) {}
+        : leftBnd(std::move(_leftBnd)), rightBnd(std::move(_rightBnd)), scanLine(std::move(_scanLine)), levels({{}}), cost(0) {}
 
     MatchResult &digitMR(int pos) { return matchResults[pos]; }
     double &consistencyCostBeforePos(int pos) { return consistencyCosts[pos-1]; }
